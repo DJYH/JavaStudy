70e93f94fb94ab52aad62299c39f199304eb7258
영훈 <dudgns0227@kookmin.ac.kr>
2016-11-23 18:05:13 +0900

    first commit

---
+++ JavaStudy/src/BubbleSort.java
@@ -0,0 +1,27 @@
+/**
+import java.util.*;
+
+public class BubbleSort {
+	
+	
+	public static void main(String args[]) {
+		
+		int[] list = {10,9,8,7,6,5,4,3,2,1};
+		
+		for(int i = 0 ; i < list.length; i++) {
+			for (int j = 0 ; j < list.length-1;j ++) {
+				if(list[j] > list[j+1] ) {
+					
+					int temp = list[j];
+					list[j] = list[j+1];
+					list[j+1] = temp;
+				}
+			}
+		}
+		for (int i =0 ; i < list.length;i ++) {
+			System.out.print(list[i]);
+		}
+	}
+	
+}
+**/
---
+++ JavaStudy/src/QuickCoding03.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class QuickCoding03 {
+	static int int1;
+	static int int2;
+	
+	public static void main (String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("2개의 정수를 입력해주세요");
+		
+		String string1 = scan.next();
+		String string2 = scan.next();
+		
+		try {
+			int1 = Integer.parseInt(string1);
+			int2 =Integer.parseInt(string2);
+			int int3  = int1/int2;
+			if(int1 >= 1000 || int2 >= 1000 ) {
+				throw new Exception();
+			}
+			System.out.println(int3);
+			
+		}catch(ArithmeticException x) {
+			System.out.println("0으로 나눌수 없습니다.");
+		}catch(NumberFormatException y) {
+			System.out.println("정수만 입력해 주세요.");
+		}catch(Exception e) {
+			System.out.println("1000 미안의 숫자만 입력해주세요.");
+		}
+	}
+	
+}
